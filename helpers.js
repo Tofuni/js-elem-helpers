96f543136f8c59b7f6f519546c59abf491a18ada
Tofuni <fran@fbacarisas.xyz>
2021-02-06 01:32:00 -0500

    add content files

---
+++ helpers.js
@@ -0,0 +1,34 @@
+// hide an element (e.g. closing a banner)
+var hide_elem = function(elem) {
+	document.getElementById(elem).style.display = 'none';
+};
+
+// pseudo-href (e.g. apply hyperlink to a <td> element)
+var navi = function(path) {
+	location.href = path;
+}
+
+// vertical show/hide toggle via button
+var displayToggleButton = function(toggleElem, toggleButton) {
+	button = document.getElementById(toggleButton);
+	elem = document.getElementById(toggleElem);
+	state = elem.style.display;
+	if (state === "none") {
+		elem.style.display = "inline-block";
+		button.innerHTML = "▲";
+	} else {
+		elem.style.display = "none";
+		button.innerHTML = "▼";
+	}
+}
+
+// display toggle for generic html element
+var displayToggleElem = function(toggleElem) {
+	elem = document.getElementById(toggleElem);
+	state = elem.style.display;
+	if (state === "none") {
+		elem.style.display = "inline-block";
+	} else {
+		elem.style.display = "none";
+	}
+};
